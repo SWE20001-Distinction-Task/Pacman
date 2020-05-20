08db2f227dec0050e86183da177cde957cfe2a3a
101227936 <101227936@students.swinburne.edu.my>
2020-05-20 22:47:10 +0800

    initial commit

---
+++ TestPacman/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPacman")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPacman")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99079f4a-fc91-455c-8566-9ef6e41f62be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestPacman/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestPacman
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestDefault()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestAssignment()
+        {
+            int i = 2;
+            Assert.AreEqual(i, 2);
+        }
+    }
+}
