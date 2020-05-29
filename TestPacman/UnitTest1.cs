b5c632cc34a7160800bb28e95effc13a23266a84
101227936 <101227936@students.swinburne.edu.my>
2020-05-29 09:49:36 +0800

    Add unit test for file read and write

--- TestPacman/UnitTest1.cs
+++ TestPacman/UnitTest1.cs
@@ -2,9 +2,11 @@
 using System.IO;
 using System.Collections.Generic;
 using NUnit.Framework;
+using System.Reflection;
 
 namespace TestPacman
 {
+
     [TestFixture]
     public class UnitTest1
     {
@@ -23,7 +25,7 @@ namespace TestPacman
         public void TestWriteFile()
         {
             Pacman_Zagorschi_Franco.Form1 testform1 = new Pacman_Zagorschi_Franco.Form1();
-            string fullPath = Directory.GetCurrentDirectory() + "/testscore.txt";
+            var fullPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/testscore.txt";
             testform1.writescore(fullPath, "Testing", 100);
             Assert.IsTrue(File.Exists(fullPath));
             File.Delete(fullPath);
@@ -32,7 +34,7 @@ namespace TestPacman
         public void TestCheckFileExist()
         {
             Pacman_Zagorschi_Franco.Form1 testform1 = new Pacman_Zagorschi_Franco.Form1();
-            string fullPath = Directory.GetCurrentDirectory() + "/testscore.txt";
+            var fullPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/testscore.txt";
             testform1.writescore(fullPath, "Testing", 100);
             testform1.writescore(fullPath, "Testing1", 200);
             Assert.IsTrue(File.Exists(fullPath));
@@ -42,7 +44,7 @@ namespace TestPacman
         public void TestCountRecord()
         {
             Pacman_Zagorschi_Franco.Form1 testform1 = new Pacman_Zagorschi_Franco.Form1();
-            string fullPath = Directory.GetCurrentDirectory() + "/testscore.txt";
+            var fullPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/testscore.txt";
             testform1.writescore(fullPath, "Testing", 100);
             Assert.AreEqual(testform1.countscorerecord(fullPath), 1);
             File.Delete(fullPath);
