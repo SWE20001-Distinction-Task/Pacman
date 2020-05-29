8d19742cf643bc793c9e8287d143b1d434a103b8
101227936 <101227936@students.swinburne.edu.my>
2020-05-29 10:33:59 +0800

    Add unit test for file read and write

--- TestPacman/UnitTest1.cs
+++ TestPacman/UnitTest1.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
 using NUnit.Framework;
-using System.Reflection;
 
 namespace TestPacman
 {
@@ -10,17 +7,17 @@ namespace TestPacman
     [TestFixture]
     public class UnitTest1
     {
-        //[Test]
-        //public void TestDefault()
-        //{
-        //    Assert.Pass();
-        //}
-        //[Test]
-        //public void TestAssignment()
-        //{
-        //    int i = 2;
-        //    Assert.AreEqual(i, 2);
-        //}
+        [Test]
+        public void TestDefault()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestAssignment()
+        {
+            int i = 2;
+            Assert.AreEqual(i, 2);
+        }
         [Test]//CCL
         public void TestSupermod()
         {
